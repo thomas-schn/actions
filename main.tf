f3e41168f571bb96ad3f1496a292b9ded48b4f0a
Thomas Schnieders <thomas.schnieders@wps.de>
2022-04-02 09:48:14 +0200

    backend auf azure

---
+++ main.tf
@@ -0,0 +1,29 @@
+terraform {
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.0.2"
+    }
+  }
+  required_version = "~> 1.1.3"
+
+  backend "azurerm" {
+    resource_group_name  = "tfstates"
+    storage_account_name = "flowatfstorageaccount"
+    container_name       = "tfstate"
+    key                  = "flowa.tfstate"
+    use_msi              = true
+  }
+
+}
+
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "thomas-state-backend-demo" {
+  name     = "git-hiub-actions-group"
+  location = "West Europe"
+}
